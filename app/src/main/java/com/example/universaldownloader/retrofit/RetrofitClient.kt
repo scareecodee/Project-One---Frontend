4f9ca0ef549d74dfe76f0aed53ec5ce1405bd7de
kumar sundram <93932085+scareecodee@users.noreply.github.com>
2025-06-30 07:06:44 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/example/universaldownloader/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/universaldownloader/retrofit/RetrofitClient.kt
@@ -13,21 +13,21 @@ object RetrofitClient {
     }
 
     val okHttpClient = OkHttpClient.Builder()
-        .connectTimeout(60, TimeUnit.SECONDS) // increase to 60 seconds
+        .connectTimeout(60, TimeUnit.SECONDS) // increase to 60 seconds ---> command ipconfig in cmd prompt
         .readTimeout(60, TimeUnit.SECONDS)
         .writeTimeout(60, TimeUnit.SECONDS)
         .addInterceptor(logging)
         .build()
 
     val retrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.75.197:8000")
+        .baseUrl("http://192.123.75.137:8000")   // your ipv4 address ---> command ipconfig in cmd prompt
         .addConverterFactory(GsonConverterFactory.create())
         .client(okHttpClient)
         .build()
 
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.75.197:8000")
+            .baseUrl("http://192.128.71.198:8000")  // your ipv4 address ---> command ipconfig in cmd prompt
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
@@ -36,14 +36,14 @@ object RetrofitClient {
 
     val videoApi: VideoApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.75.197:8000")
+            .baseUrl("http://192.138.75.1974:8000")   // your ipv4 address ---> command ipconfig in cmd prompt
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(VideoApiService::class.java)
     }
     val audioapi: AudioApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.75.197:8000")
+            .baseUrl("http://192.168.75.194:8000")   // your ipv4 address ---> command ipconfig in cmd prompt
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
